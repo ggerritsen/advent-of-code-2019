b088c3fb98431b65dee8db88851f965417e09c95
ggerritsen <444568+ggerritsen@users.noreply.github.com>
2019-12-27 22:04:40 +0100

    Day 6 part 2

--- 6/main.go
+++ 6/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -22,13 +23,15 @@ func main() {
 		input = append(input, sc.Text())
 	}
 
-	log.Printf("orbit map checksum: %d", run(input))
-
+	log.Printf("orbit map checksum: %d", calculateNumOrbits(input))
 	log.Printf("End part 1")
 
+	log.Printf("Start part 2")
+	log.Printf("min transfers to Santa: %d", minOrbitTransfers(input))
+	log.Printf("End part 2")
 }
 
-func run(s []string) int {
+func calculateNumOrbits(s []string) int {
 	orbitMap := map[string]string{}
 	for _, o := range s {
 		ss := strings.Split(o, ")")
@@ -45,4 +48,39 @@ func run(s []string) int {
 	}
 
 	return numOrbits
+}
+
+func minOrbitTransfers(s []string) int {
+	orbitMap := map[string]string{}
+	for _, o := range s {
+		ss := strings.Split(o, ")")
+		orbitMap[ss[1]] = ss[0]
+	}
+
+	var transfers []int
+
+	target := "SAN"
+	iteration := -1
+outer:
+	for target != "COM" {
+		iteration++
+		numTransfers := iteration
+		current := orbitMap["YOU"]
+		target = orbitMap[target]
+		for current != target {
+			// do 1 transfer
+			current = orbitMap[current]
+			numTransfers++
+
+			// arrived at COM - this is not a viable route, try next iteration
+			if current == "COM" {
+				continue outer
+			}
+		}
+		// arrived at target! Store and continue next iteration
+		transfers = append(transfers, numTransfers)
+	}
+
+	sort.Ints(transfers)
+	return transfers[0]
 }
--- 6/main_test.go
+++ 6/main_test.go
@@ -14,8 +14,27 @@ func Test_run(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := run(tt.orbitMap); got != tt.want {
-				t.Errorf("run() = %v, want %v", got, tt.want)
+			if got := calculateNumOrbits(tt.orbitMap); got != tt.want {
+				t.Errorf("calculateNumOrbits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_minOrbitTransfers(t *testing.T) {
+	tests := []struct {
+		name string
+		orbitMap []string
+		want int
+	}{
+		{"same orbit", []string{"COM)B","B)C","C)D","D)E","E)F","B)G","G)H","D)I","E)J","J)K","K)L","I)YOU","I)SAN"}, 0},
+		{"1 step", []string{"COM)B","B)C","C)D","D)E","E)F","B)G","G)H","D)I","E)J","J)K","K)L","D)YOU","I)SAN"}, 1},
+		{"simple", []string{"COM)B","B)C","C)D","D)E","E)F","B)G","G)H","D)I","E)J","J)K","K)L","K)YOU","I)SAN"}, 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minOrbitTransfers(tt.orbitMap); got != tt.want {
+				t.Errorf("minOrbitTransfers() = %v, want %v", got, tt.want)
 			}
 		})
 	}
