a71a1677bfb7be64d47e01c3034766f6e85227ab
ggerritsen <444568+ggerritsen@users.noreply.github.com>
2019-12-30 23:10:43 +0100

    Implemented Heap's algorithm for generating permutations

--- 7/main.go
+++ 7/main.go
@@ -1,21 +1,113 @@
 package main
 
 import (
+	"encoding/csv"
 	"log"
+	"os"
+	"strconv"
 )
 
 func main() {
-	
+	log.Printf("Start")
+
+	f, err := os.Open("/Users/ggerritsen/dev/personal/advent-of-code-2019/7/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	s, err := csv.NewReader(f).Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	input := make([]int, len(s))
+	for i := 0; i < len(s); i++ {
+		ss, err := strconv.Atoi(s[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+		input[i] = ss
+	}
+
+	// keep a copy
+	original := make([]int, len(input))
+	for i := 0; i < len(s); i++ {
+		original[i] = input[i]
+	}
+
+	log.Printf("Start part 1")
+
+	_, o := runIntcode(input, []int{3, 0})
+	log.Printf("Output: %d", o)
+
+	log.Printf("End part 1")
 }
 
-func runIntcode(i []int, input int) ([]int, int) {
+func findOptimalPhases(opCodes []int) []int {
+	return []int{0}
+}
+
+// source: https://en.wikipedia.org/wiki/Heap%27s_algorithm
+//procedure generate(k : integer, A : array of any):
+//    if k = 1 then
+//        output(A)
+//    else
+//        // Generate permutations with kth unaltered
+//        // Initially k == length(A)
+//        generate(k - 1, A)
+//
+//        // Generate permutations for kth swapped with each k-1 initial
+//        for i := 0; i < k-1; i += 1 do
+//            // Swap choice dependent on parity of k (even or odd)
+//            if k is even then --> gg: removed this if, seems to cause a bug (in a 3-elem list, the 2nd element never gets put in the 3rd place)
+//                swap(A[i], A[k-1]) // zero-indexed, the kth is at k-1
+//            else
+//                swap(A[0], A[k-1])
+//            end if
+//            generate(k - 1, A)
+//
+//        end for
+//    end if
+func generatePermutations(k int, a []int) [][]int {
+	if k == 1 {
+		return [][]int{a}
+	}
+
+	c := generatePermutations(k-1, a)
+
+	aa := make([]int, len(a))
+	copy(aa, a)
+	for i := 0; i < k-1; i++ {
+		tmp := aa[i]
+		aa[i] = aa[k-1]
+		aa[k-1] = tmp
+
+		aaa := make([]int, len(aa))
+		copy(aaa, aa)
+		c = append(c, generatePermutations(k-1, aaa)...)
+	}
+
+	return c
+}
+
+func runAmplifiers(intCodes []int, phases []int) int {
+	output := 0
+	for i := 0; i < len(phases); i++ {
+		_, output = runIntcode(intCodes, []int{phases[i], output})
+	}
+
+	return output
+}
+
+func runIntcode(i []int, input []int) ([]int, int) {
 	result := make([]int, len(i))
 	for j := 0; j < len(i); j++ {
 		result[j] = i[j]
 	}
 	output := 0
 
-	index := 0
+	index, inputCount := 0, 0
 	operator, modes := parseOperator(result[index])
 	for operator != 99 {
 		if operator == 1 || operator == 2 {
@@ -47,8 +139,9 @@ func runIntcode(i []int, input int) ([]int, int) {
 		// input
 		if operator == 3 {
 			resultLocation := result[index+1]
-			result[resultLocation] = input
+			result[resultLocation] = input[inputCount]
 			index = index + 2
+			inputCount++
 		}
 		// output
 		if operator == 4 {
--- 7/main_test.go
+++ 7/main_test.go
@@ -9,7 +9,7 @@ func Test_run(t *testing.T) {
 	tests := []struct {
 		name   string
 		i      []int
-		input  int
+		input  []int
 		result []int
 		output int
 	}{
@@ -29,7 +29,8 @@ func Test_run(t *testing.T) {
 		{name: "equals-2", i: []int{8, 5, 6, 5, 99, 8, 8}, result: []int{8, 5, 6, 5, 99, 1, 8}},
 		{name: "equals-3", i: []int{1108, 55, 8, 1, 99}, result: []int{1108, 0, 8, 1, 99}},
 		{name: "equals-4", i: []int{1108, 8, 8, 1, 99}, result: []int{1108, 1, 8, 1, 99}},
-		{name: "i/o", i: []int{3, 0, 4, 0, 99}, input: 55, result: []int{55, 0, 4, 0, 99}, output: 55},
+		{name: "i/o", i: []int{3, 0, 4, 0, 99}, input: []int{55}, result: []int{55, 0, 4, 0, 99}, output: 55},
+		{name: "i/o multiple inputs", i: []int{3, 0, 3, 3, 4, 3, 99}, input: []int{1, 55}, result: []int{1, 0, 3, 55, 4, 3, 99}, output: 55},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
@@ -62,6 +63,78 @@ func Test_parseOperand(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got1, tt.want1) {
 				t.Errorf("parseOperator() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func Test_runAmplifiers(t *testing.T) {
+	type args struct {
+		i      []int
+		phases []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"simple", args{[]int{3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0}, []int{4, 3, 2, 1, 0}}, 43210},
+		{"simple-2", args{[]int{3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0}, []int{0, 1, 2, 3, 4}}, 54321},
+		{"simple-3", args{[]int{3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0}, []int{1, 0, 4, 3, 2}}, 65210},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := runAmplifiers(tt.args.i, tt.args.phases); got != tt.want {
+				t.Errorf("runAmplifiers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_findOptimalPhases(t *testing.T) {
+	tests := []struct {
+		name     string
+		intCodes []int
+		want     []int
+	}{
+		{"simple", []int{3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0}, []int{4, 3, 2, 1, 0}},
+		//{"simple-2", args{[]int{3,23,3,24,1002,24,10,24,1002,23,-1,23, 101,5,23,23,1,24,23,23,4,23,99,0,0}, []int{0,1,2,3,4}}, 54321},
+		//{"simple-3", args{[]int{3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33, 1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0}, []int{1,0,4,3,2}}, 65210},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findOptimalPhases(tt.intCodes); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findOptimalPhases() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_generatePermutations(t *testing.T) {
+	tests := []struct {
+		name string
+		k    int
+		a    []int
+		want [][]int
+	}{
+		{"simple", 1, []int{1}, [][]int{{1}}},
+		{"simple-2", 2, []int{1, 2}, [][]int{{1, 2}, {2, 1}}},
+		{"simple-3", 3, []int{1, 2, 3}, [][]int{{1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := generatePermutations(tt.k, tt.a)
+			if len(got) != len(tt.want) {
+				t.Errorf("len = %d, want %d", len(got), len(tt.want))
+			}
+		outer:
+			for _, v := range tt.want {
+				for _, v2 := range got {
+					if reflect.DeepEqual(v, v2) {
+						continue outer
+					}
+				}
+				t.Errorf("did not find %v, (got %v)", v, got)
 			}
 		})
 	}
