7a08ac61492a6f97eb337f35ea86f99aea1326be
ggerritsen <444568+ggerritsen@users.noreply.github.com>
2020-02-05 09:42:41 +0100

    Day 1 part 1 WIP #2

--- 12/main.go
+++ 12/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"strconv"
 	"strings"
@@ -10,8 +11,8 @@ func main() {
 
 }
 
-func parse(s []string) []*moon {
-	result := make([]*moon, len(s))
+func parse(s []string) []moon {
+	result := make([]moon, len(s))
 	for i, ss := range s {
 		result[i] = parseLine(ss)
 	}
@@ -19,7 +20,7 @@ func parse(s []string) []*moon {
 	return result
 }
 
-func parseLine(s string) *moon {
+func parseLine(s string) moon {
 	// <x=8, y=0, z=8>
 	s2 := strings.Trim(s, "<>")
 	ss := strings.Split(s2, ",")
@@ -45,13 +46,68 @@ func parseLine(s string) *moon {
 		log.Fatal(err)
 	}
 
-	return &moon{x, y, z}
+	return moon{pos: position{x, y, z}}
+}
+
+func iterate(moons []moon) []moon {
+	f := func(i int) int {
+		if i == 0 {
+			return 0
+		}
+		if i < 0 {
+			return -1
+		}
+		return 1
+	}
+
+	// apply gravity
+	for i := 0; i < len(moons); i++ {
+		this := &moons[i]
+		for j := 0; j < len(moons); j++ {
+			if i == j {
+				continue
+			}
+			other := moons[j]
+			this.vel.x += f(other.pos.x - this.pos.x)
+			this.vel.y += f(other.pos.y - this.pos.y)
+			this.vel.z += f(other.pos.z - this.pos.z)
+		}
+	}
+
+	// apply velocity
+	for i := 0; i < len(moons); i++ {
+		moons[i].pos.x += moons[i].vel.x
+		moons[i].pos.y += moons[i].vel.y
+		moons[i].pos.z += moons[i].vel.z
+	}
+
+	return moons
+}
+
+func newMoon(posX, posY, posZ, velX, velY, velZ int) moon {
+	return moon{
+		pos: position{x: posX, y: posY, z: posZ},
+		vel: velocity{x: velX, y: velY, z: velZ},
+	}
 }
 
 type moon struct {
+	pos position
+	vel velocity
+}
+
+func (m *moon) String() string {
+	return fmt.Sprintf("%v", *m)
+}
+
+type position struct {
 	x, y, z int
 }
 
-func run() {
+type velocity struct {
+	x, y, z int
+}
+
+func run(m []*moon) {
 
 }
--- 12/main_test.go
+++ 12/main_test.go
@@ -9,15 +9,44 @@ func Test_parse(t *testing.T) {
 	tests := []struct {
 		name string
 		input []string
-		want []*moon
+		want []moon
 	} {
-		{"simple", []string{"<x=8, y=0, z=8>"}, []*moon{{8,0,8}}},
+		{"simple", []string{"<x=8, y=0, z=8>"}, []moon{{pos: position{8,0,8}}}},
+		{"extended", []string{"<x=8, y=0, z=8>", "<x=0, y=-5, z=-10>"}, []moon{{pos: position{8,0,8}}, {pos:position{0, -5, -10}}}},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got, want := parse(tt.input), tt.want; !reflect.DeepEqual(got, want) {
 				t.Errorf("parse() = %v, want %v", got, want)
+			}
+		})
+	}
+}
+
+func Test_iteration(t *testing.T) {
+	tests := []struct {
+		name string
+		input []moon
+		want []moon
+	} {
+		{"simple", []moon{
+			newMoon(-1, 0, 2, 0, 0, 0),
+			newMoon(2, -10, -7, 0, 0, 0),
+			newMoon(4, -8, 8, 0, 0, 0),
+			newMoon(3, 5, -1, 0, 0, 0),
+		}, []moon{
+			newMoon(2, -1, 1, 3,-1, -1),
+			newMoon(3, -7, -4, 1,3, 3),
+			newMoon(1, -7, 5, -3,1, -3),
+			newMoon(2, 2, 0, -1,-3, 1),
+		}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got, want := iterate(tt.input), tt.want; !reflect.DeepEqual(got, want) {
+				t.Errorf("iterate() = %v, want %v", got, want)
 			}
 		})
 	}
